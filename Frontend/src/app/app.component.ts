524b5476a6d37125cf89b46265f781e265d9a2eb
Valentino Nicola <valenicola02@gmail.com>
2024-11-22 23:00:08 -0300

    Home Admin Page add

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Frontend';
+  title = 'LaElsi';
+  menuOption: string = '';
+
+  onOption(menuOption: string) {
+    this.menuOption = menuOption;
+  }
 }
