ddc6505d1f39656cb5b8ca8d35adce6816f8eec6
Valentino Nicola <valenicola02@gmail.com>
2024-11-26 19:35:02 -0300

    delete old frontend

--- Frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
-import { AdminComponent } from './admin/admin.component';
-
-export const routes: Routes = [
-  { path: '', component: IniciarSesionComponent },
-  { path: 'adminhome', component: AdminComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
-];
